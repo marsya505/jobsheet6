91901dc20be4cc34d36dfccfefcc5671fb264950
marsya505 <marsyazulfazahrani0@gmail.com>
2024-10-08 10:04:49 +0700

    commit prac 6

---
+++ Selection2Exp311.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+/**
+ * Selection2Exp311
+ */
+public class Selection2Exp311 {
+    public static void main(String[] args) {
+        Scanner input11 = new Scanner(System.in);
+        String category;
+        int income, netSalary;
+        double tax = 0;
+        System.out.print("Input category = ");
+        category = input11.nextLine();
+        System.out.print("Input income = ");
+        income = input11.nextInt();
+        if (category.equalsIgnoreCase("worker")){
+            if(income <= 2000000)
+                tax = 0.1;
+            else if(income <= 3000000)
+                tax = 0.2;
+            netSalary = (int) (income - (tax*income));
+            System.out.println("Nett salary = "+netSalary);
+        }else if(category.equals("Businessman")){
+            if(income <= 2500000)
+                tax = 0.15;
+            else if(income <= 3500000)
+                tax = 0.2;
+            else 
+                tax = 0.25;
+            netSalary = (int) (income - (tax*income));
+            System.out.println("Nett salary = "+netSalary);
+        }else 
+            System.out.println("Invalid category!");
+    }
+}
